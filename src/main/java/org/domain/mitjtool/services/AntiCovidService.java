dc136a42f03acef017d633727f97917edaa6c31b
tienan.chien <tienan.chien@iisigroup.com>
2021-05-18 15:48:45 +0800

    fix issue.

--- src/main/java/org/domain/mitjtool/services/AntiCovidService.java
+++ src/main/java/org/domain/mitjtool/services/AntiCovidService.java
@@ -1,8 +1,8 @@
 package org.domain.mitjtool.services;
 
 import java.time.LocalDate;
-import java.time.Period;
 import java.time.format.TextStyle;
+import java.time.temporal.ChronoUnit;
 import java.time.temporal.TemporalAdjusters;
 import java.util.ArrayList;
 import java.util.List;
@@ -18,14 +18,16 @@ public class AntiCovidService {
 		LocalDate eDate = sDate.plusMonths(1)
 				               .with(TemporalAdjusters.lastDayOfMonth());
 		List<WorkDTO> dtos = new ArrayList<>();
-		while (sDate.isBefore(eDate)) {
+		int i = 0;
+		while (sDate.plusDays(i).isBefore(eDate)) {
+			LocalDate t = sDate.plusDays(i);
 			WorkDTO dto = new WorkDTO();
-			dto.setDate(sDate);
-			dto.setWeek(sDate.getDayOfWeek().getDisplayName(TextStyle.FULL, Locale.TAIWAN));
-			dto.setA(Period.between(WorkDTO.START_DATE_A, sDate).getDays() % 2 == 0);
-			dto.setB(Period.between(WorkDTO.START_DATE_B, sDate).getDays() % 2 == 0);
-			sDate = sDate.plusDays(1);
+			dto.setDate(t);
+			dto.setWeek(t.getDayOfWeek().getDisplayName(TextStyle.FULL, Locale.TAIWAN));
+			dto.setA(ChronoUnit.DAYS.between(WorkDTO.START_DATE_A, t) % 2 == 0);
+			dto.setB(ChronoUnit.DAYS.between(WorkDTO.START_DATE_B, t) % 2 == 0);
 			dtos.add(dto);
+			i++;
 		}
 		return dtos;
 	}
